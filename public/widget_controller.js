372c3989553a0eb2b68406b86d2805060aa0b560
Noelle Barber <barberossa@gmail.com>
2017-03-14 15:31:15 -0500

    refactor

--- public/widget_controller.js
+++ public/widget_controller.js
@@ -2,12 +2,18 @@ $('document').ready(function(){
   // $('#markdown-box').on("submit", function(event){
   //   event.preventDefault();
   // })
-    $('#markdown-text').keyup(function(){
-      var input = $(this).val()
-      var transformable = new Widget()
-      var transformedInput = transformable.transform(input)
-
-      $('#preview').html(transformedInput)
-      })
+      widgetController.load();
 
 })
+
+var transformable = new Widget();
+var widgetController = {
+  load: function(){
+    widgetView.input().keyup(this.runProcess);
+  },
+  runProcess: function(){
+    var input = $(this).val();
+    var transformedInput = transformable.transform(input);
+    widgetView.output().html(transformedInput);
+  }
+}
---
+++ public/widget_view.js
@@ -0,0 +1,4 @@
+var widgetView = {
+  input: function(){ return $('#markdown-text'); },
+  output: function(){ return $('#preview'); }
+}
